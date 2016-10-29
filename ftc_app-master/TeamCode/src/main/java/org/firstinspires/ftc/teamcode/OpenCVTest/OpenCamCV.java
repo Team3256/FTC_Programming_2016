1e9e9957b383f5cdcafa937fef20644e20c041ba
Team 3256 <Team 3256>
2016-10-28 20:36:22 -0700

    Added OpenCv libraries

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest/OpenCamCV.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.OpenCVTest;
+import android.app.Activity;
+
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+/**
+ * Created by Team 2891 on 10/28/2016.
+ */
+public class OpenCamCV extends Activity{
+    
+}
