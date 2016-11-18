48cec10fd0782e1104a3081bd799ec992c32f317
Bo887 <ezhang887@gmail.com>
2016-11-18 15:20:58 -0800

    add coments

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoBlueBeacons.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoBlueBeacons.java
@@ -34,28 +34,31 @@ public class AutoBlueBeacons extends LinearOpMode{
         robot.driveTrain.oneWheelTurn(34, 0.275, true);
         //drive to white line
         robot.driveTrain.driveToLine(60, 0.9);
-        //wait half a second
         //drive a tiny bit forward so we are centered when we turn
         robot.driveTrain.driveToDistance(2.75, 0.4, true);
-        //wait half a second
         //turn to the beacon
         robot.driveTrain.turn(37,0.25,true);
-        //drive forward to see color sensor
+        //set servo position depending on what color we see
+        robot.beacon.setServoPosition();
         //hit beacon
         robot.driveTrain.driveToDistance(8,0.35, true);
-        //drive backward
+        //drive backward so we can turn towards second beacon
         robot.driveTrain.driveToDistance(6,0.5, false);
-
+        //reset servo positions to neutral for both
         robot.beacon.initPos();
+        //turn towards the second beacon
         robot.driveTrain.turn(77,0.4,false);
+        //drive forward to prepare to see to the line
         robot.driveTrain.driveToDistance(40,0.8,true);
+        //drive forward to the line
         robot.driveTrain.driveToLine(12,0.6);
+        //drive forward a tiny bit so we are centered when we turn
         robot.driveTrain.driveToDistance(2.5,0.4,true);
+        //turn to the beaon
         robot.driveTrain.turn(80,0.3,true);
+        //set servo positions depending on what we see
+        robot.beacon.setServoPosition();
+        //hit beacon
         robot.driveTrain.driveToDistance(8,0.35,true);
-        /*
-        beac2blue = robot.isBlue();
-        robot.driveTrain.driveToDistance(-6,0.2);
-        robot.driveTrain.driveToDistance(6,0.2);*/
     }
 }
